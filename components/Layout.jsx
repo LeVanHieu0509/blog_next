fc2467108d35f90b7d1d62792f9767ac6c3acc58
LeVanHieu0509 <levanhieu2k1.dz@gmail.com>
2022-06-27 19:29:06 +0700

    fix bug

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,13 +1,24 @@
-import Head from "next/head";
-import React from "react";
-import Header from "./Header";
+import Header from "./layouts/components/Header";
+import Banner from "./layouts/components/Banner";
+import Sidebar from "./layouts/components/Sidebar";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Header />
-
-      {children}
+      <div className="wrapper-box" style={{ position: "relative" }}>
+        <Header />
+        <Banner />
+        <div className="content" style={{ width: "1170px", margin: "auto" }}>
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+            <div className="lg:col-span-8 col-span-1">{children}</div>
+            <div className="lg:col-span-4 col-span-1">
+              <div className="lg:stickey relative top-8">
+                <Sidebar />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
